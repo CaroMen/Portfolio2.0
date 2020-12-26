aae998645babf7ca597f627695424fd2e73c7cb8
Caro <55109108+CaroMen@users.noreply.github.com>
2020-12-25 20:49:42 -0600

    new page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar, Footer } from './components';
 import Home from './pages/homePage/Home';
+import ModernBF from './pages/mbfPage';
 import GlobalStyle from './globalStyles';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar></Navbar>
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/modernbyfrancis' exact component={ } />
       </Switch>
       <Footer />
       <GlobalStyle></GlobalStyle>
---
+++ src/pages/mbfPage/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data';
+import { InfoSection } from '../../components';
+
+const Home = () => {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default Home
